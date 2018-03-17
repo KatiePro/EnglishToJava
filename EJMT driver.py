1f9a0be2cc669522f04a2f892330900f63414444
caitlin.mcelwee13@gmail.com <NbtK5PlPg79s>
2018-03-17 12:53:25 -0400

    Put into repo
    It's now in the repo, yay. No changes

---
+++ EJMT driver.py
@@ -0,0 +1,82 @@
+class Node:
+    def __init__(self,initdata):
+        self.data = initdata
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self,newdata):
+        self.data = newdata
+
+    def setNext(self,newnext):
+        self.next = newnext
+
+
+class UnorderedList:
+
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head == None
+
+    def add(self,item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+
+        return count
+
+    def search(self,item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+
+        return found
+
+    def remove(self,item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+            
+commands = UnorderedList()
+more = True
+while (more == True):
+    String = input("Input a string to process: \n")
+    commands.add(String)
+    answer = input("Enter y to continue: ")
+    if answer == 'y':
+        more = True
+    else:
+        more = False
+print('Strings entered so far:')
+Node = commands.head
+for x in range(0, commands.size()):
+    print(Node.data)
+    Node = Node.getNext()
