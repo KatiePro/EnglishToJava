3b1af29add02ca975db0b6d679a96a90188217f8
prochilok3 <kathrynprochilo@gmail.com>
2018-04-13 00:21:56 -0400

    Added return statements for extra return on the product

--- EJMT All Return Statements.py
+++ EJMT All Return Statements.py
@@ -1,12 +1,1 @@
-# Obviously I didn't actually do that.
-
-# @Abigail don't worry about this, I'm making a joke at Katie.
-
-# Anyway, here's the file I took a picture of earlier.
-
-# P is the set of productions of A -> a, meaning:
-
-# S -> AdfgBC
-# A -> aaaB | /\
-# B -> b | bB
-# C -> ccc | cCc | /\
+╚(ಠ_ಠ)=┐
